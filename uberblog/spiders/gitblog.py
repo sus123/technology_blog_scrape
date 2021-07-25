2436b69116fc04f66fe15158fc3446e2ee7fb2af
Sushma <sushma.shrestha50@gmail.com>
2021-07-25 18:36:05 +1000

    Git blog added in the list

---
+++ uberblog/spiders/gitblog.py
@@ -0,0 +1,19 @@
+import scrapy
+from scrapy.loader import ItemLoader
+
+from uberblog.items import UberblogItem
+
+
+class GitBlogSpider(scrapy.Spider):
+    name = 'gitblogspider'
+    allowed_domains = ['github.blog']
+    start_urls = ['https://github.blog/']
+
+    def parse(self, response, **kwargs):
+        for title_selector in response.css('.post-item__title > a'):  # [a:first , a:second]
+            title_item = ItemLoader(item=UberblogItem(), selector=title_selector)
+            title_item.add_css('title', '::text')
+            yield title_item.load_item()
+
+        for next_page in response.css('a.all-posts__view-more'):  # [a:first]
+            yield response.follow(next_page, self.parse)
